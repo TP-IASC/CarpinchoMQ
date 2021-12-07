f5fb79198d11248834d90935ed89c2f683a30122
dpeccia <diego.peccia@10pines.com>
2021-12-06 23:23:49 -0300

    adding a setup_all for creating the nodes and making the rpc call prettier

--- test/carpincho_mq_test.exs
+++ test/carpincho_mq_test.exs
@@ -2,10 +2,22 @@ defmodule CarpinchoMQTest do
   use ExUnit.Case
   doctest Queue
 
-  test "initial state of a just created queue is correct" do
+  setup_all do
     [node1, node2] = LocalCluster.start_nodes("my-cluster", 2)
-    {:ok, {primary_queue_pid, replica_queue_pid}} = :rpc.call(node1, Queue, :new, [:cola1, 345, :publish_subscribe])
+    {:ok, %{node1: node1, node2: node2, run: &execute(node1, &1, &2, &3)}}
+  end
+
+  def execute(node, module, function, args) do
+    :rpc.call(node, module, function, args)
+  end
+
+  test "initial state of a just created queue is correct", state do
+    run = state[:run]
     
-    assert :rpc.call(node1, Queue, :state, [:cola1]) == %PrimaryQueue{elements: [], max_size: 345, name: :cola1, subscribers: [], work_mode: :publish_subscribe}
+    {:ok, _} = run.(Queue, :new, [:cola1, 345, :publish_subscribe])
+    actual_initial_state = run.(Queue, :state, [:cola1])
+    
+    expected_initial_state = %PrimaryQueue{elements: [], max_size: 345, name: :cola1, subscribers: [], work_mode: :publish_subscribe}
+    assert expected_initial_state == actual_initial_state
   end
 end
