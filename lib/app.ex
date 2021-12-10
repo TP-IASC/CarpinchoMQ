6537eec65b2da1e47dd0bc1f9415682aa4de1661
CimimUxMaio <cimimuxmaio@gmail.com>
2021-12-10 14:20:09 -0300

    emprolijo app.ex con hijos extra dependiendo del ambiente

--- lib/app.ex
+++ lib/app.ex
@@ -3,33 +3,26 @@ defmodule App do
   require Logger
 
   def start(_type, _args) do
-    if _args == [] do
-      topologies = Application.get_env(:libcluster, :topologies)
-
-      children = [
-        { Cluster.Supervisor, [topologies, [name: App.ClusterSupervisor]] },
-        App.HordeRegistry,
-        { App.HordeSupervisor, [strategy: :one_for_one, distribution_strategy: AvoidReplica, process_redistribution: :active] },
-        App.NodeObserver.Supervisor
-      ]
-
-      Supervisor.start_link(children, strategy: :one_for_one)
-    else
-      http_port = Enum.fetch!(System.argv, 0) |> String.to_integer
-      udp_port = Enum.fetch!(System.argv, 1) |> String.to_integer
-
-      topologies = Application.get_env(:libcluster, :topologies)
-
-      children = [
-        { Cluster.Supervisor, [topologies, [name: App.ClusterSupervisor]] },
-        App.HordeRegistry,
-        { App.HordeSupervisor, [strategy: :one_for_one, distribution_strategy: AvoidReplica, process_redistribution: :active] },
-        App.NodeObserver.Supervisor,
-        { HTTPServerSupervisor, http_port },
-        { UDPServerSupervisor, udp_port }
-      ]
-
-      Supervisor.start_link(children, strategy: :one_for_one)
+    extra_childs = case Mix.env do
+      :test -> []
+      _ ->
+        http_port = Enum.fetch!(System.argv, 0) |> String.to_integer
+        udp_port = Enum.fetch!(System.argv, 1) |> String.to_integer
+        [
+          { HTTPServerSupervisor, http_port },
+          { UDPServerSupervisor, udp_port }
+        ]
     end
+
+    topologies = Application.get_env(:libcluster, :topologies)
+
+    children = [
+      { Cluster.Supervisor, [topologies, [name: App.ClusterSupervisor]] },
+      App.HordeRegistry,
+      { App.HordeSupervisor, [strategy: :one_for_one, distribution_strategy: AvoidReplica, process_redistribution: :active] },
+      App.NodeObserver.Supervisor,
+    ] ++ extra_childs
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
